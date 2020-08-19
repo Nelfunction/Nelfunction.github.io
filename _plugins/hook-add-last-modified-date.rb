53a305bf7644e8002cce10cfd9d70f029ad6af1c
Nelfunction <oat641@gmail.com>
2020-08-19 16:54:31 +0900

    hooks not work?

--- _plugins/hook-add-last-modified-date.rb
+++ _plugins/hook-add-last-modified-date.rb
@@ -1,7 +1,13 @@
-Jekyll::Hooks.register :posts, :pre_render do |post|
+Jekyll::Hooks.register :docs, :pre_render do |post|
+
+  # debug
+  puts "Firing :docs, :pre_render from : " + File.basename(__FILE__) + " for : " + post.relative_path
 
   # get the current post last modified time
   modification_time = File.mtime( post.path )
+
+  # debug
+  puts "modification_time = " + modification_time.strftime('%A, %B %dth %Y at %l:%M%p')
 
   # inject modification_time in post's datas.
   post.data['last-modified-date'] = modification_time
