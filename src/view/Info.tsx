1170be9ef349220f6e863125b179c63ba60d891a
Nelfunction <oat641@gmail.com>
2022-11-22 08:40:27 +0900

    테스트

--- src/view/Info.tsx
+++ src/view/Info.tsx
@@ -1,10 +1,28 @@
-import { Box, Container } from '@chakra-ui/react';
-import { Outlet } from 'react-router-dom';
+import { Box, Button, Input } from '@chakra-ui/react';
+import axios from 'axios';
+import { useState } from 'react';
 
 export const Info = () => {
+  const [data, setData] = useState<any>();
+  const [key, setKey] = useState<string>('');
+
+  const githubUserAPI = async () => {
+    axios
+      .get('https://api.github.com/user', {
+        headers: {
+          Authorization: 'Bearer ' + key,
+        },
+      })
+      .then(res => {
+        setData(JSON.stringify(res.data));
+      });
+  };
+
   return (
     <Box h="120vh" bg="blue.200">
-      정보
+      <Input onChange={e => setKey(e.target.value)} />
+      <Button onClick={githubUserAPI}>Send</Button>
+      {data ? <Box>{data}</Box> : <Box>empty</Box>}
     </Box>
   );
 };
