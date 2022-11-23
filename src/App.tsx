e856d0223de648906af1db1b5003ca844c035aae
이길상 <oat641@gmail.com>
2022-11-23 17:59:04 +0900

    Feat : Pretected Route

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,22 @@ import { Home } from './view/Home';
 import { Info } from './view/Info';
 import { Editor } from './view/Editor';
 import { Article } from './view/Article';
+import { useSelector } from 'react-redux';
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Routes>
-      <Route path="/*" element={<Layout />}>
-        <Route path="home" element={<Home />} />
-        <Route path="info" element={<Info />} />
-        <Route path="article/view/*" element={<Article />} />
-        <Route path="editor" element={<Editor />} />
-        <Route index element={<Navigate to="home" replace />} />
-      </Route>
-    </Routes>
-  </ChakraProvider>
-);
+export const App = () => {
+  const authToken = useSelector((state: any) => state.ghAPIReducer.authToken);
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Routes>
+        <Route path="/*" element={<Layout />}>
+          <Route path="home" element={<Home />} />
+          <Route path="info" element={<Info />} />
+          <Route path="article/view/*" element={<Article />} />
+          <Route path="article/edit" element={authToken ? <Editor /> : <Navigate to="/home" />} />
+          <Route index element={<Navigate to="home" replace />} />
+        </Route>
+      </Routes>
+    </ChakraProvider>
+  );
+};
--- src/view/Editor.tsx
+++ src/view/Editor.tsx
@@ -1,10 +1,13 @@
 import { Box, Button, Input } from '@chakra-ui/react';
 import { useState } from 'react';
 import MDEditor from '@uiw/react-md-editor';
+import { useDispatch, useSelector } from 'react-redux';
 
 export const Editor = () => {
+  const dispatch = useDispatch();
+  const authToken = useSelector((state: any) => state.ghAPIReducer.authToken);
+
   const [markdown, setMarkdown] = useState<string>('');
-  const [key, setKey] = useState<string>('');
 
   document.documentElement.setAttribute('data-color-mode', 'light');
 
